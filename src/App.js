a97877989d08f5a6a9db1c777012e6a0cd25d18d
ANKIT <85980606+ankit94a@users.noreply.github.com>
2023-01-24 08:15:48 +0530

    24/1/23

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+  increment = () => {
+    this.setState({ count: this.state.count + 1 });
+    
+  };
+
+  decrement = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
+  render() {
+    return (
+      <div className="container">
+        <h1 className="heading ">
+          Simple{" "}
+          <spam className="text-3xl font-bold underline bg-blend-color-dodge">
+            Inc
+          </spam>
+          rement And <spam className="text-red-300">Dec</spam>rement value using
+          Buttons
+        </h1>
+        <div className="flex items-center ...">
+          <button onClick={this.increment} className="btn">
+            +
+          </button>
+          <h1 className="font-sans md:font-serif">{this.state.count}</h1>
+          <button onClick={this.decrement} className="btn">
+            -
+          </button>
+        </div>
+        
+      </div>
+    );
+  }
 }
 
 export default App;
